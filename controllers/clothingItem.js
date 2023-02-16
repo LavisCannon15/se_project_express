721838510f7aef1bc58b83dea0a6b71e1496c78d
LavisCannon15 <85091518+LavisCannon15@users.noreply.github.com>
2023-02-16 15:53:43 -0500

    Add files via upload

---
+++ controllers/clothingItem.js
@@ -0,0 +1,49 @@
+const ClothingItem = require("../models/clothingItem");
+
+const createItem = (req, res) => {
+  console.log(req);
+  console.log(req.body);
+
+  const { name, weather, imageURL } = req.body;
+
+  ClothingItem.create({ name, weather, imageURL })
+    .then((item) => {
+      console.log(item);
+      res.send({ data: item });
+    })
+    .catch((e) => {
+      res.status(500).send({ message: "Error: createItem failed", e });
+    });
+};
+
+const getItems = (req, res) => {
+  ClothingItem.find({})
+    .then((items) => res.status(200).send(items))
+    .catch((e) => {
+      res.status(500).send({ message: "Error: getItems failed", e });
+    });
+};
+
+const updateItem = (req, res) => {
+  const { itemId } = req.params;
+  const { imageURL } = req.body;
+
+  ClothingItem.findByIdAndUpdate(itemId, { $set: { imageURL } })
+    .orFail()
+    .then((item) => res.status(200).send({ data: item }))
+    .catch((e) => {
+      res.status(500).send({ message: "Error: updateItems failed", e });
+    });
+};
+
+const deleteItem = (req, res) => {
+  const { itemId } = req.params;
+  ClothingItem.findByIdAndDelete(itemId)
+    .orFail()
+    .then((item) => res.status(204).send({}))
+    .catch((e) => {
+      res.status(500).send({ message: "Error: updateItems failed", e });
+    });
+};
+
+module.exports = { createItem, getItems, updateItem, deleteItem };
