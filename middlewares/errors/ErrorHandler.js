af906298d76f584be1964a41cd87bcc0cf08e6b6
LavisCannon15 <85091518+LavisCannon15@users.noreply.github.com>
2023-04-07 14:41:08 -0400

    Add files via upload

--- middlewares/errors/ErrorHandler.js
+++ middlewares/errors/ErrorHandler.js
@@ -1,7 +1,8 @@
 const errorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Error has occured on server';
-  next(res.status(statusCode).send({ message }));
+  const message = err.message || "Error has occured on server";
+  res.status(statusCode).send({ message });
+  next(); 
 };
 
 module.exports = errorHandler;
